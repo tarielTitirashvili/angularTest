cb40cc508784a94bb3acd4e8d89e8c88265759c0
tarielTitirashvili <tarieltitirashvili@gmail.com>
2023-05-16 15:19:47 +0400

    ngFor & ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
+interface Data{
+  id: number
+  name: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public data: Data[] =[
+    {
+      id: 0,
+      name: 'tariel'
+    },
+    {
+      id: 1,
+      name: 'lasha'
+    },    
+    {
+      id: 2,
+      name: 'dali'
+    },
+  ]
   title = 'testAngular';
 }
